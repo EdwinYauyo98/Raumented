e35f98d4a7d6ae4b9108d5f3014ef36af8529acd
Edwin <eyauyos@uni.pe>
2021-06-14 22:40:15 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(express.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Edwin's 3000." });
+});
+
+// set port, listen for requests
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
