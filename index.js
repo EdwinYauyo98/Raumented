7e800c056b723ce1f70157ee180ceed7d4fe3d5d
Edwin <eyauyos@uni.pe>
2021-06-14 23:53:11 -0500

    a

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 5000
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/diegogay', function (req, res) {
+  res.send('si papi confirmo');
+});
+
+
+
 app.listen(PORT, function () {
   console.log('Example app listening on port 5000!');
 });
