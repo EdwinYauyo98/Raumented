b9091041e7cda3e3ff0158d4bb278f63df65ec39
Diego Bergna Aguilar <77370322+Diego-Bergna@users.noreply.github.com>
2021-07-22 01:41:19 -0500

    agregando una ruta

--- routes/seller-routes.js
+++ routes/seller-routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const {addSeller,
       loginSeller,
-      editSeller,
-      getSeller
+      editSeller
 } = require('../controllers/sellerController');
 
 const router = express.Router();
@@ -10,7 +9,6 @@ const router = express.Router();
 router.post('/addseller', addSeller);
 router.post('/loginSeller', loginSeller);
 router.put('/editSeller/:idSeller', editSeller);
-router.get('/getSeller/:idSeller', getSeller);
 
 module.exports = {
     routes: router
